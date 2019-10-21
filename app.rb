6954a44f0f32647dfabe313605c5a051510c5b74
Alexander Ilyin <alexander.ilyin11@gmail.com>
2019-10-21 22:15:01 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,91 +1,79 @@
 print "Сколько вам лет? "
 age = gets.to_i
 
+if age < 18
+  puts "Вам еще нет 18 лет!"
+  sleep 3
+  exit
+end
+
 print "Хотите играть? (Y/N) "
 answer = gets.strip.capitalize
 
-if age >= 18 && answer == "Y"
+if answer == "Y"
   puts "Хорошо, поиграем!"
 
   money = 100
 
-  1000.times do
+  while true do
 
-    puts "Введите Enter, чтобы дернуть ручку"
+    puts "Нажмите Enter, чтобы дернуть ручку"
     gets
 
     x = rand(0..9)
     y = rand(0..9)
     z = rand(0..9)
 
-    # 000
+    puts "Комбинация"
 
-    if x == 0 && y == 0 && z == 0
+    # Рандомное значение x
+    10.times do |xx|
+      number = rand(0..9)
+      if xx < 9
+        print number
+        print "\r"
+        sleep 0.08
+      else
+        print x
+      end
+    end
+
+    puts
+
+    # Рандомное значение y
+    10.times do |yy|
+      number = rand(0..9)
+      if yy < 9
+        print number
+        print "\r"
+        sleep 0.08
+      else
+        print y
+      end
+    end
+
+    puts
+
+    # Рандомное значение z
+    10.times do |zz|
+      number = rand(0..9)
+      if zz < 9
+        print number
+        print "\r"
+        sleep 0.08
+      else
+        print z
+      end
+    end
+
+    puts
+
+    if x == y && x == z && x == 0 # 000
       puts "Ваш баланс обнулен!"
       money = 0
-    end
-
-    # 111
-
-    if x == 1 && y == 1 && z == 1
-      puts "Вам зачислено 10 долларов"
-      money = money + 10
-    end
-
-    # 222
-
-    if x == 2 && y == 2 && z == 2
-      puts "Вам зачислено 20 долларов"
-      money = money + 20
-    end
-
-    # 333
-
-    if x == 3 && y == 3 && z == 3
-      puts "Вам зачислено 30 долларов"
-      money = money + 30
-    end
-
-    # 444
-
-    if x == 4 && y == 4 && z == 4
-      puts "Вам зачислено 40 долларов"
-      money = money + 40
-    end
-
-    # 555
-
-    if x == 5 && y == 5 && z == 5
-      puts "Вам зачислено 50 долларов"
-      money = money + 50
-    end
-
-    # 666
-
-    if x == 6 && y == 6 && z == 6
-      puts "Вам зачислено 60 долларов"
-      money = money + 60
-    end
-
-    # 777
-
-    if x == 7 && y == 7 && z == 7
-      puts "Вам зачислено 70 долларов"
-      money = money + 70
-    end
-
-    # 888
-
-    if x == 8 && y == 8 && z == 8
-      puts "Вам зачислено 80 долларов"
-      money = money + 80
-    end
-
-    # 999
-
-    if x == 9 && y == 9 && z == 9
-      puts "Вам зачислено 90 долларов"
-      money = money + 90
+    elsif x == y && x == z # xxx
+      puts "Вам зачислено #{10 * x} долларов"
+      money = money + 10 * x
     end
 
     puts "Осталось денег: #{money} долларов"
